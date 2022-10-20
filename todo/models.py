bfedb0ac743536de33ed3147a1ee762ffc0aecb2
TarsisGermano <tarsisx10@gmail.com>
2022-10-19 23:12:30 -0300

    comentado o modelos para evitar bugs

--- todo/models.py
+++ todo/models.py
@@ -3,8 +3,5 @@ from django.db import models
 class Item(models.Model):
     content = models.TextField()
 
-Class GameDev(models.Model):
-    nome =  models.CharField(max_length = 255)
-    data_nascimento = models.DateField()
-    empresa = models.CharField(max_length = 255)
+#
 
